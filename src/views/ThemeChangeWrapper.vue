b19d15b45b7596099055c76617581a01887b7774
Reda <herradi.r@gmail.com>
2023-03-30 15:09:25 +0200

    Added theme values TODO: add design for button

---
+++ src/views/ThemeChangeWrapper.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="single-page-wrapper"
+        :class="theme">
+        <div class="theme-change" @click="changeTheme"></div>
+        <slot />
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ThemeChangeWrapper",
+    props: {},
+    data() {
+        return {
+            theme: 'light'
+        };
+    },
+    methods: {
+        changeTheme: function() {
+            if(this.theme == 'light') this.theme = 'dark';
+            else this.theme = 'light';
+            console.log(this.theme)
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.theme-change {
+    color: black;
+    position: fixed;
+    top: 0;
+    right: 5%;
+    height: 50px;
+    width: 10px;
+    background-color: black;
+    cursor: pointer;
+}
+</style>
